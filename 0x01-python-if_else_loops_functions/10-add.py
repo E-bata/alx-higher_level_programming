9431cecd42a0a067a2ebf9f958d09e0a3c1981ac
E-bata <emmilytatas@gmail.com>
2023-06-06 07:12:37 -0700

    python if-statement

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def add(a, b):
+    """Return the addition of a and b respectively."""
+    return (a + b)
---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def pow(a, b):
+    """returns the power of a and b respectively"""
+    return (a ** b)
