cba5eb7ae25de14095f4de671fa00c46b03d63bc
root <emmilytatas@gmail.com>
2023-07-21 19:40:48 +0000

    update

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -2,7 +2,6 @@
 # test_rectangle.py
 # Brennan D Baraban <375@holbertonschool.com>
 """Defines unittests for models/rectangle.py.
-
 Unittest classes:
     TestRectangle_instantiation - line 25
     TestRectangle_width - line 114
@@ -456,7 +455,6 @@ class TestRectangle_stdout(unittest.TestCase):
     @staticmethod
     def capture_stdout(rect, method):
         """Captures and returns text printed to stdout.
-
         Args:
             rect (Rectangle): The Rectangle to print to stdout.
             method (str): The method to run on rect.
