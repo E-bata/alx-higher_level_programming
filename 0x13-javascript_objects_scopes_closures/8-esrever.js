bd85fd7495e38c1b300092dc83352de8b68087c0
E-bata <emmilytatas@gmail.com>
2023-09-13 12:41:52 -0700

    update

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
-exports.nbOccurences = function (list, searchElement) {
-  return (list.filter(elem => elem === searchElement).length);
+exports.esrever = function (list) {
+  const arr = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    arr.push(list[i]);
+  }
+  return (arr);
 };
